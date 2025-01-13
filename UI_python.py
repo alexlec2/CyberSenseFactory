0c560ea48c596653c7472fe57fa3189ae4bcd72d
alexlec2 <leclercalex3@gmail.com>
2025-01-13 11:18:00 +0100

    update with input vitesse

--- UI_python.py
+++ UI_python.py
@@ -2,6 +2,8 @@ from tkinter import Tk, Button, PhotoImage, Label
 from PIL import Image, ImageTk  # Importation pour redimensionner les images
 from base import connect_to_arduino, disconnect_from_arduino, init_relay_output
 from controlAiguillage import activate_relay
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 # Initialisation
 board = None
@@ -43,17 +45,24 @@ def toggle_relay(button, relays, images, state):
 
     # Active le relais alternatif
     relay_to_activate = relays[state["relay"]]
-    print("Relay activé : ", relay_to_activate)
     activate_relay(board, relay_to_activate, time_sleep)
-
+    print("Relai activé :", relay_to_activate)
+    
     # Alterne entre les deux relais
     state["relay"] = 1 - state["relay"]
+
+# Affichage de la vitesse sélectionnée avec un slider
+def update_vitesse(slider, slider_value_label):
+    """Mise à jour du label de vitesse en fonction de la valeur du slider."""
+    vitesse = int(slider.get())  # Obtient la valeur du slider
+    slider_value_label.config(text=f"Vitesse: {vitesse}%")
+
 
 # Création de l'interface utilisateur
 def create_ui():
     root = Tk()
     root.title("Contrôle Aiguillages")
-    root.geometry("350x500")  # Taille adaptée pour les boutons et légendes
+    root.geometry("350x600")  # Taille adaptée pour les boutons et légendes
 
     # Chargement et redimensionnement des images
     max_width = 300
@@ -86,6 +95,21 @@ def create_ui():
     # Légende pour l'aiguillage 2
     label_aiguillage_2 = Label(root, text="Aiguillage 2", font=("Arial", 14))
     label_aiguillage_2.pack(pady=5)
+
+    # Création du slider de vitesse
+    frame_slider = ttk.Frame(root)
+    frame_slider.pack(pady=10)
+
+    # Slider avec plage de -100% à +100%
+    vitesse_slider = ttk.Scale(
+        frame_slider, from_=-100, to=100, value=0, length=200, orient="horizontal",
+        command=lambda val: update_vitesse(vitesse_slider, slider_value_label)  # Mise à jour du label
+    )
+    vitesse_slider.pack()
+    
+    # Affichage de la valeur du slider
+    slider_value_label = Label(frame_slider, text="Vitesse: 0%", font=("Arial", 14))
+    slider_value_label.pack()
 
     # Démarrage de la boucle principale de l'interface
     root.mainloop()
