3cb70c6ba469bc36eb220457604521640131bbe9
alexlec2 <leclercalex3@gmail.com>
2025-01-14 18:27:59 +0100

    update final

--- base.py
+++ base.py
@@ -1,21 +1,20 @@
 from pyfirmata2 import Arduino, util
 import serial.tools.list_ports
 
-def find_arduino(name):
-    """Détecte automatiquement le port de l'Arduino et retourne son nom de port."""
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        if name in port.description or "tty" in port.device or "COM" in port.device:
-            return port
-    raise IOError("Aucun Arduino détecté. Vérifiez la connexion.")
+# def find_arduino(name):
+#     """Détecte automatiquement le port de l'Arduino et retourne son nom de port."""
+#     ports = serial.tools.list_ports.comports()
+#     for port in ports:
+#         if name in port.description or "tty" in port.device or "COM" in port.device:
+#             return port
+#     raise IOError("Aucun Arduino détecté. Vérifiez la connexion.")
 
 def connect_to_arduino(name):
     """Établit la connexion à l'Arduino."""
     try:
         # port = find_arduino(name)
         port = Arduino.AUTODETECT
-        print(port)
-        print(f"Connexion en cours avec l'Arduino {port} sur le port {port}...")
+        print(f"Connexion en cours avec l'Arduino sur le port {port}...")
         board = Arduino(port)
         print(f"Connecté avec succès à l'Arduino {port}.\n")
         return board
